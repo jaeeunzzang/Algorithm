8707ba060389746b0a2195c837e59fdd71998e00
jenny <qkrwodms0201@gmail.com>
2020-12-28 16:38:26 +0900

    #5 arr_2577

---
+++ BOJ/배열/Arr_2577.java
@@ -0,0 +1,28 @@
+package BOJ.배열;
+
+import java.util.Scanner;
+
+public class Arr_2577 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10]; // 배열선언
+        // int a = 150, b = 266, c = 427;
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(); // 정수입력받기
+        int mul = a * b * c; // 곱하기
+        // System.out.println("mul:" + mul);
+        while (true) {
+            if (mul / 10 == 0) { // 10으로 나눴을때 몫이 0이면 루프종료
+                arr[mul] += 1;
+                break;
+            }
+            int mod = mul % 10; // 나머지값 (배열인덱스로 들어갈 값)
+            arr[mod] += 1;
+            mul = mul / 10;
+        }
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+
+}
