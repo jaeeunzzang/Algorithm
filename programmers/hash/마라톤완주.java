975e954b25613ff1e466bbba8b228c027ce92561
jenny <qkrwodms0201@gmail.com>
2021-01-02 18:47:42 +0900

    update code array

--- programmers/hash/마라톤완주.java
+++ programmers/hash/마라톤완주.java
@@ -2,26 +2,22 @@ package programmers.hash;
 
 import java.util.Arrays;
 
-import javax.swing.text.DefaultStyledDocument.ElementSpec;
-
 public class 마라톤완주 {
 
     public String solution(String[] participant, String[] completion) {
         // 효율성을 고민해봅시다.
-        String answer = "";
+
         Arrays.sort(participant); // 정렬 후에 찾기
         Arrays.sort(completion);
 
         for (int i = 0; i < completion.length; i++) {
+
             if (!completion[i].equals(participant[i])) {
-                answer = participant[i];
-            }
-            if (completion[i].equals(participant[i])) {
-                continue;
+                return participant[i];
             }
         }
 
-        return answer;
+        return participant[participant.length - 1];
     }
 
     public static void main(String[] args) {
