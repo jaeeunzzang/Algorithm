082d7c76a13ce38fee7507a13352ece078fee880
jenny <qkrwodms0201@gmail.com>
2021-01-02 18:00:51 +0900

    update

--- programmers/hash/마라톤완주.java
+++ programmers/hash/마라톤완주.java
@@ -4,10 +4,25 @@ public class 마라톤완주 {
 
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+
+        for (int i = 0; i < participant.length; i++) { // 4명
+            for (int j = 0; j < completion.length; j++) {
+                if (participant[i] != null && participant[i].equals(completion[j])) {
+                    participant[i] = "";
+                    completion[i] = "";
+                }
+            }
+        }
+        for (String s : participant) {
+            System.out.println(s);
+        }
         return answer;
     }
 
     public static void main(String[] args) {
-
+        마라톤완주 fun = new 마라톤완주();
+        String[] participant = { "mislav", "stanko", "mislav", "ana" };
+        String[] completion = { "stanko", "mislav", "ana" };
+        fun.solution(participant, completion);
     }
 }
