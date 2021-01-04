fef256a5019067255a1d39c8df75406201d34c05
jenny <qkrwodms0201@gmail.com>
2021-01-04 14:03:38 +0900

    update

--- programmers/Level4/쿠키구입.java
+++ programmers/Level4/쿠키구입.java
@@ -2,6 +2,14 @@ package programmers.Level4;
 
 public class 쿠키구입 {
     public int solution(int[][] busket, int[] cookie) {
-        int answer=0;
+        int answer = 0;
         return answer;
+    }
+
+    public static void main(String[] args) {
+        쿠키구입 fc = new 쿠키구입();
+        int[][] busket = {};
+        int[] cookie = {};
+        fc.solution(busket, cookie);
+    }
 }
