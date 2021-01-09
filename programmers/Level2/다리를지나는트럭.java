333b96c4729f1011adf21bd328005eb30e344bba
jenny <qkrwodms0201@gmail.com>
2021-01-09 15:58:44 +0900

    update #19

--- programmers/Level2/다리를지나는트럭.java
+++ programmers/Level2/다리를지나는트럭.java
@@ -7,7 +7,7 @@ public class 다리를지나는트럭 {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0; // 걸리는 시간
         int weight_sum = 0; // 다리위에 올라와있는 트럭의 무게 합
-        Queue<Integer> bridge = new LinkedList<Integer>(); // que 구현은 linked list로 한다.
+        Queue<Integer> bridge = new LinkedList<Integer>(); // queue 구현은 linked list로 한다.
         // 현재 다리위에 올라와있는 트럭들을 저장할 bridge 생성.
 
         for (int i = 0; i < truck_weights.length; i++) { // 트럭 갯수만큼 반복
@@ -16,11 +16,13 @@ public class 다리를지나는트럭 {
                     bridge.add(truck_weights[i]); // 트럭을 추가해준다.
                     weight_sum += truck_weights[i]; // 무게를 더해주고
                     answer++; // 시간 증가
+                    System.out.println(answer + "초:" + bridge);
                     break; // 루프 탈출 . 다음 트럭 시작
                 }
 
                 if (bridge.size() == bridge_length) { // 큐(다리)가 다리길이만큼 차 있는경우.
                     weight_sum -= bridge.poll();// 다리에서 트럭을 빼주고 그 무게만큼 sum에서 빼준다.
+                    System.out.println(answer + "초:" + bridge);
                     /* poll: 가장 앞에 들어간 값을 반환하고 제거 */
 
                 }
@@ -28,13 +30,17 @@ public class 다리를지나는트럭 {
                     bridge.add(truck_weights[i]); // 큐에 트럭을 넣고
                     answer++; // 시간을 증가
                     weight_sum += truck_weights[i];
+                    System.out.println(answer + "초:" + bridge);
                     break; // 다음트럭 시작
                 } else if (weight_sum + truck_weights[i] > weight) { // 제한무게를 넘을때
                     answer++;
                     bridge.add(0); // 0무게 추가. 큐 size는 증가, 무게는 변화없음.
+                    System.out.println(answer + "초:" + bridge);
                 }
+
             }
         }
+        // answer: 마지막 트럭이 다리에 진입한 시간
         return answer + bridge_length; // 다리길이만큼 시간 더해준다.
     }
 
