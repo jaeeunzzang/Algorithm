9b04c43cb6d6db0f714b7021fa47fdaae7240436
jenny <qkrwodms0201@gmail.com>
2021-01-09 14:00:03 +0900

    #19 init

---
+++ programmers/Level2/다리를지나는트럭.java
@@ -0,0 +1,17 @@
+package programmers.Level2;
+
+public class 다리를지나는트럭 {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        다리를지나는트럭 fc = new 다리를지나는트럭();
+        int bridge_length = 0;
+        int weight = 0;
+        int[] truck_weights = {};
+
+        fc.solution(bridge_length, weight, truck_weights);
+    }
+}
