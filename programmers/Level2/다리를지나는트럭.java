0658ddd044d4c871ad47f6ca36c3f27027d5c958
jenny <qkrwodms0201@gmail.com>
2021-01-09 14:23:01 +0900

    #19 update

--- programmers/Level2/다리를지나는트럭.java
+++ programmers/Level2/다리를지나는트럭.java
@@ -2,16 +2,17 @@ package programmers.Level2;
 
 public class 다리를지나는트럭 {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
+        int answer = 0; // 걸리는 시간
+
         return answer;
     }
 
     public static void main(String[] args) {
         다리를지나는트럭 fc = new 다리를지나는트럭();
-        int bridge_length = 0;
-        int weight = 0;
-        int[] truck_weights = {};
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = { 7, 4, 5, 6 };
 
-        fc.solution(bridge_length, weight, truck_weights);
+        System.out.println(fc.solution(bridge_length, weight, truck_weights));
     }
 }
