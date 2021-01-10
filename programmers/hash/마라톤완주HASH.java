d9698a1fae4b4b096f3590ca3978ca3bfa8c30ab
jenny <qkrwodms0201@gmail.com>
2021-01-10 12:02:31 +0900

    update

--- programmers/hash/마라톤완주HASH.java
+++ programmers/hash/마라톤완주HASH.java
@@ -5,15 +5,15 @@ import java.util.HashMap;
 public class 마라톤완주HASH {
     public String solution(String[] participant, String[] completion) {
         HashMap<Object, String> hm = new HashMap<>();
-        for (String s : participant) {
-            hm.put(hashCode(), s);
+        for (int i = 0; i < participant.length; i++) {
+            hm.put(i, participant[i]);
+            System.out.println(hm.get(i));
         }
-        System.out.println(hm);
         return "";
     }
 
     public static void main(String[] args) {
-        마라톤완주 fun = new 마라톤완주();
+        마라톤완주HASH fun = new 마라톤완주HASH();
         String[] participant = { "mislav", "stanko", "mislav", "ana" };
         String[] completion = { "stanko", "mislav", "ana" };
         System.out.println(fun.solution(participant, completion));
