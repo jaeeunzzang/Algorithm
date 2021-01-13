d6b1c5bcc87df22cca98bb5fa1283b4656abdd1a
박재은 <50936779+jaeeunzzang@users.noreply.github.com>
2021-01-13 17:15:03 +0900

    Update 마라톤완주HASH.java

--- programmers/hash/마라톤완주HASH.java
+++ programmers/hash/마라톤완주HASH.java
@@ -3,19 +3,35 @@ package programmers.hash;
 import java.util.HashMap;
 
 public class 마라톤완주HASH {
-    public String solution(String[] participant, String[] completion) {
-        HashMap<Object, String> hm = new HashMap<>();
-        for (int i = 0; i < participant.length; i++) {
-            hm.put(i, participant[i]);
-            System.out.println(hm.get(i));
-        }
-        return "";
-    }
+   public String solution(String[] participant, String[] completion) {
 
-    public static void main(String[] args) {
-        마라톤완주HASH fun = new 마라톤완주HASH();
-        String[] participant = { "mislav", "stanko", "mislav", "ana" };
-        String[] completion = { "stanko", "mislav", "ana" };
-        System.out.println(fun.solution(participant, completion));
-    }
+		HashSet<String> set = new HashSet<>();
+
+		for (int i = 0; i < participant.length; i++) {
+			if (set.isEmpty()) {
+				set.add(participant[i]);
+				continue;
+			}
+			if (set.contains(participant[i])) {
+				return participant[i]; // 이미 있으면 중복된거닉가 중복리턴
+			}
+			set.add(participant[i]); // add 해주고
+		}
+		for(int i=0;i<completion.length;i++) {
+			if(set.contains(completion[i])) {
+				set.remove(completion[i]);
+			}
+		}
+		Iterator<String> it=set.iterator();
+		return it.next();
+
+		//return participant[length - 1];
+	}
+
+	public static void main(String[] args) {
+		Hash fun = new Hash();
+		String[] participant = { "marina", "josipa", "nikola", "vinko", "filipa"};
+		String[] completion = { "josipa", "filipa", "marina", "nikola"};
+		System.out.println(fun.solution(participant, completion));
+	}
 }
