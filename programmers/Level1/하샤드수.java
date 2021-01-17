a3ca1982528218fa89d695c0ab35a64c7fd7f444
jenny <qkrwodms0201@gmail.com>
2021-01-17 22:27:31 +0900

    update

--- programmers/Level1/하샤드수.java
+++ programmers/Level1/하샤드수.java
@@ -1,14 +1,30 @@
 package programmers.Level1;
 
+import java.util.Scanner;
+
 public class 하샤드수 {
 
     public boolean solution(int x) {
         boolean answer = true;
+        int arr[] = {};
+        int cnt = 0;
+        int sum = 0;
+        while (true) {
+            if (x == 0) {
+                break;
+            }
+            sum += x % 10;
+            x = x / 10;
+            cnt++;
+        }
+        System.out.println(sum);
         return answer;
     }
 
     public static void main(String[] args) {
         하샤드수 fc = new 하샤드수();
-
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(fc.solution(x));
     }
 }
