80008f240cc401f3b4a88fdd00c218c283bd5048
jenny <qkrwodms0201@gmail.com>
2021-01-17 22:19:31 +0900

    #21 init

---
+++ programmers/Level1/하샤드수.java
@@ -0,0 +1,14 @@
+package programmers.Level1;
+
+public class 하샤드수 {
+
+    public boolean solution(int x) {
+        boolean answer = true;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        하샤드수 fc = new 하샤드수();
+
+    }
+}
