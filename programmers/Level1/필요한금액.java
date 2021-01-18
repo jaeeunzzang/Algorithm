4173398e2542660623a7dc890e7e850d20419245
jenny <qkrwodms0201@gmail.com>
2021-01-18 14:34:31 +0900

    init #20

---
+++ programmers/Level1/필요한금액.java
@@ -0,0 +1,19 @@
+package programmers.Level1;
+
+public class 필요한금액 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        for (int i = 0; i < d.length; i++) {
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        필요한금액 fc = new 필요한금액();
+        int[] d = { 1, 3, 2, 5, 4 };
+        int budget = 9;
+        System.out.println(fc.solution(d, budget));
+    }
+
+}
