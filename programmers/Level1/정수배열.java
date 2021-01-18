ffc53101ebafde58073c90fe36fb0b0bca3980cc
jenny <qkrwodms0201@gmail.com>
2021-01-18 17:37:14 +0900

    update

--- programmers/Level1/정수배열.java
+++ programmers/Level1/정수배열.java
@@ -1,6 +1,8 @@
 package programmers.Level1;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class 정수배열 {
     public HashSet<Integer> solution(int[] numbers) {
@@ -12,13 +14,14 @@ public class 정수배열 {
                 set.add(numbers[i] + numbers[j]);
             }
         }
-
+        List li = new ArrayList<>(set);
+        // li.sort(asc);
         return set;
     }
 
     public static void main(String[] args) {
         정수배열 fc = new 정수배열();
-        int[] numbers = { 5, 0, 2, 7, 1 };
+        int[] numbers = { 0, 0 };
         System.out.println(fc.solution(numbers));
     }
 
