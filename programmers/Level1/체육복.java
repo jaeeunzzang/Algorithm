b11e0161b8293edaf140a7d4cb21d97848af9784
jenny <qkrwodms0201@gmail.com>
2021-01-22 14:21:33 +0900

    update

--- programmers/Level1/체육복.java
+++ programmers/Level1/체육복.java
@@ -10,8 +10,8 @@ public class 체육복 {
     public static void main(String[] args) {
         체육복 fc = new 체육복();
         int n = 5;
-        int[] lost = { 2, 4 };
-        int[] reverse = { 1, 3, 5 };
+        int[] lost = { 2, 4 }; // 체육복을 잃어버린 학생 번호
+        int[] reverse = { 1, 3, 5 }; // 여벌체육복을 가진 학생 번호
         System.out.println(fc.solution(n, lost, reverse));
     }
 
