4a2953ed65adb915a9c8a664f8557e73b032ef7e
jenny <qkrwodms0201@gmail.com>
2021-01-22 14:20:03 +0900

    init #22

---
+++ programmers/Level1/체육복.java
@@ -0,0 +1,15 @@
+package programmers.Level1;
+
+public class 체육복 {
+    public int solution(int n, int[] lost, int[] reverse) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        체육복 fc = new 체육복();
+        System.out.println(fc.solution(n, lost, reverse));
+    }
+
+}
