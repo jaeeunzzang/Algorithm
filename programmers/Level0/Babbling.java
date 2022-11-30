7b8dea70a1be4ed04af9cfd058898a6a15a5b9e3
jenny <qkrwodms0201@gmail.com>
2022-11-30 22:13:26 +0900

    레벨 추가

---
+++ programmers/Level0/Babbling.java
@@ -0,0 +1,12 @@
+package programmers.Level0;
+
+public class Babbling{
+   public int solution(){
+       int answer = 0;
+
+       return answer;
+   }
+    public static void main(String[]args){
+
+        }
+}
