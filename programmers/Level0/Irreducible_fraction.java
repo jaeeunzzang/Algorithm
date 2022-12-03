c55e150a3c5a0cf5d4d46cfc90293ffd150b61a5
jenny <qkrwodms0201@gmail.com>
2022-12-03 16:28:42 +0900

    머리가 안돌아가네 일단 여기까지,,ㅋㅋ

--- programmers/Level0/Irreducible_fraction.java
+++ programmers/Level0/Irreducible_fraction.java
@@ -7,43 +7,44 @@ package programmers.Level0;
 
 //처음부터 분모가 같은경우일수도있음.
 //최대공약수, 최소공배수 구해서 접근
+// 최대공약수 : 두 수를 나눠서 나머지가 0 인것중에 젤 큰수...
+// 최소공배수 : (num1 * num2) / 최대공약수
 public class Irreducible_fraction {
+    static Irreducible_fraction fn = new Irreducible_fraction();
+
+    //최대 공약수 구하는 함수
+    public int getGcd(int num1, int num2){
+        int gcd = 0;
+        int min = Math.min(num1, num2); // (num1 < num2) ? num1 : num2
+
+        for(int i = 1; i <= min; i++){
+            if(num1 % i == 0 && num2 % i == 0) {
+                gcd = i;
+            }
+        }
+
+        return gcd;
+    }
 
     public int[] solution(int denum1, int num1, int denum2, int num2) {
         int[] answer = new int[2];
-        int num3 = num1*num2;
+        
+        int num3 = num1 * num2;
+        int gcd = fn.getGcd(num1,num2);
+        System.out.println("gcd = " + gcd);
+        int lcm = num3 / gcd;
+        System.out.println("lcm = " + lcm);
+
         int left_num = denum1 * num2;
         int right_num = denum2 * num1;
         int sum = left_num+right_num;
 
-        for(int i = 2; i<10;i++){
-            if(sum % i == 0 && num3 % i == 0){
-                answer[0] = sum/num1;
-                answer[1] = num3/num1;
-                break;
-            }else{
-               answer[0] =sum;
-                answer[1] = num3;
-            }
-        }
-
-        /*if(sum % num1 == 0 && num3 % num1 == 0){
-            answer[0] = sum/num1;
-        }else if( sum % num2 == 0 && num3 % num2 == 0){
-            answer[0] = sum/num2;
-            answer[1] = num3/num2;
-        }else{
-            answer[0] =sum;
-            answer[1] = num3;
-        }
-        */
         System.out.println(answer[0]);
         System.out.println(answer[1]);
         return answer;
     }
 
     public static void main(String[] args) {
-        Irreducible_fraction fn = new Irreducible_fraction();
-        fn.solution(9,2,1,3);
+        fn.solution(9,2,1,2);
     }
 }
