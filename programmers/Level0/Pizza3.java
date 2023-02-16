523cd934a8eeababea0a3dc4f4a6e6dec9b4ce69
jenny <qkrwodms0201@gmail.com>
2023-02-16 14:05:56 +0900

    하기실타

---
+++ programmers/Level0/Pizza3.java
@@ -0,0 +1,14 @@
+package programmers.Level0;
+
+public class Pizza3 {
+    public int solution(int slice,int n){
+        int answer=0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pizza3 fn = new Pizza3();
+        System.out.println(fn.solution(7,10));
+    }
+}
