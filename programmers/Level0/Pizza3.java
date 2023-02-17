e148720965c3c09767fa31b9ecbb85243c5cf9c5
jenny <qkrwodms0201@gmail.com>
2023-02-17 10:21:18 +0900

    나는바보,,,루저,,멍청이센척하는지질이,,,

--- programmers/Level0/Pizza3.java
+++ programmers/Level0/Pizza3.java
@@ -2,13 +2,22 @@ package programmers.Level0;
 
 public class Pizza3 {
     public int solution(int slice,int n){
-        int answer=0;
+        int answer=1;       // 피자의 갯수 ! 초기값은 1판
+        int pizza = slice;      // 현재 피자의 조각수
 
-        return answer;
+        while(true){        //조건이 참일동안 반복
+            if(pizza/n<1){      //조각수 나누기 인원수의 몫이 1보다 작을때
+                pizza +=slice;      //현재 피자의 조각수에 잘라주는 조각수를 더해주고
+                answer++;           //피자의 갯수를 1 늘려준당
+            }else{              //몫이 1이거나 그보다 클때
+                break;              //반복 그만하기
+            }
+        }
+        return answer;              //피자의 갯수 반환
     }
 
     public static void main(String[] args) {
         Pizza3 fn = new Pizza3();
-        System.out.println(fn.solution(7,10));
+        System.out.println(fn.solution(4,4));
     }
 }
