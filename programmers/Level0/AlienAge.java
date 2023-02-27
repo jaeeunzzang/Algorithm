46afda6fee9aeafd53e8984c547640e175d3bb42
jenny <qkrwodms0201@gmail.com>
2023-02-27 13:45:22 +0900

    mon

---
+++ programmers/Level0/AlienAge.java
@@ -0,0 +1,13 @@
+package programmers.Level0;
+
+public class AlienAge {
+    public String solution(int age){
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        AlienAge fn = new AlienAge();
+        System.out.println(fn.solution(20));
+    }
+}
